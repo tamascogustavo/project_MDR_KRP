f61356c4d83abc10e83ab7557be3bd0536d6874d
tamascogustavo <gustavo.tamasco@usp.br>
2021-04-05 15:47:06 -0300

    phylogeny build done

--- phylogeny.py
+++ phylogeny.py
@@ -15,7 +15,8 @@ import subprocess
 import os
 from os import listdir
 from os.path import isfile,join
-
+import shutil
+import re
 
 #functionsandclasses
 def list_directories(path):
@@ -28,74 +29,115 @@ def list_directories(path):
     return all_dirs
 
 
-def list_files(path,organism):
+def list_files(all_paths, path,organism):
     '''
     This function lists all file in a the path
+
     :param path:path of adir
     :return:all files path in a list
+
+    '''
+    complete_path = "{}/{}/ANNOT/PROKKA".format(path,organism)
+    files = list_directories(complete_path)
+    for file in files:
+        if ".fna" in file:
+            file_path = "{}/{}".format(complete_path, file)
+            all_paths.append(file_path)
+    return all_paths
+
+def print_status(files):
+    '''
+    This function prints info about the files selected
+
+    :param files: List of fna files
+    :return: None
     '''
 
-    complete_path = "{}/{}".format(path,organism)
-    directories = list_directories(complete_path)
-    print(directories)
-    #files=[fileforfileinlistdir(complete_path)ifisfile(join(complete_path,file))]
-    #print(listdir(complete_path))
+    message = "A total of {} file containing .fna in their name were found." \
+              "Next step, building a directory of genomes and running parsnp!".format(len(files))
+    print(message)
+def create_genomes_dir(path):
+    '''
+    This function creates a new directory in the local machine were the fna files will
+    be stored
+    :param path: path to the new dir
+    :return: None
+    '''
+    if os.path.exists(path):
+        print("Dir {} was already created".format(path))
+    else:
+        os.makedirs(path)
+        print("{} was created".format(path))
+
+def move_file(file_path, new_dir):
+    '''
+    This function will move the fna files to the created folder
+
+    :param bof:
+    :param new_dir:
+    :return: none
+    '''
+    out_name = file_path.strip().split("/")[-1]
+    if os.path.exists(out_name):
+        print("{} already in the dir".format(out_name))
+    else:
+        shutil.copy(file_path, new_dir)
+        print("{} was moved to the dir".format(file_path))
 
 
-def get_file_m(path):
-    """Thisfunctioncreatesthepathforallyourfiles
+def run_parsnp(dir_path,genomes_dir):
+    '''
+    This function runs parsnp
+    :param dir_path: is the location in the start of the section
+    :param genomes_dir: path to the fna files
+    :return: 
+    '''
 
-    input:isastringwiththepathtoyourfoldercontainingallfiles
-    Out:isalistofstringscontainingthepathofallfiles
-
-    Obs:nocasopegeuisoreadspoissãoosarquivosondeosreadsnãoalinhadosforamremovidos
-    """
-    files=[]
-    for r,d,f in os.walk(path):
-        for file in f:
-            if file.startswith("reads_")and"montagem"in file:
-                files.append(os.path.join(r,file))
-    return files
-
-def get_file_final(path):
-    """Thisfunctioncreatesthepathforallyourfiles
-
-    input:isastringwiththepathtoyourfoldercontainingallfiles
-    Out:isalistofstringscontainingthepathofallfiles
-
-    Obs:nocasopegeuisoreadspoissãoosarquivosondeosreadsnãoalinhadosforamremovidos
-    """
-    files = []
-    for r,d,f in os.walk(path):
-        for file in f:
-            if file.startswith("only")and"montagem"in file:
-                files.append(os.path.join(r,file))
-    return files
-
+    out_name = dir_path.split("/")[0:-1]
+    out_name = "/".join(map(str,out_name))
+    out_file = re.compile("P_*_??_??_*")
+    files = list_directories(out_name)
+    for file in files:
+        if bool(re.match(out_file, file)) == True:
+            name = file
+    out_name = "{}/{}".format(out_name,name)
+    if os.path.exists(out_name):
+        print("Parsnp was already executed, {} already exist".format(out_name))
+    else:
+        cmd_parsnp = "parsnp -r ! -d {} -c ".format(genomes_dir)
+        exit_message = subprocess.check_call(cmd_parsnp, shell=True)
+        print("Exit status: {0}".format(exit_message))
+        print("{0} Parsnp was executed".format(out_name))
 
 def main():
     """Main code of the script"""
 
     #Getthefiles
+    all_fna_file_path = []
     path_to_all_info = '/Users/gustavotamasco/Google Drive/Shared drives/Projeto MDR KRP/Dados_Sequenciamento/'
     dirpath=os.getcwd()
     os.chdir(path_to_all_info)
-    directories=list_directories(path_to_all_info)
+    directories = list_directories(path_to_all_info)
 
     '''Genomes'''
     genomes_path = "{}{}".format(path_to_all_info,directories[0])
     os.chdir(genomes_path)
     genome_dir = list_directories(genomes_path)
     for organism in genome_dir:
-        list_files(genomes_path,organism)
+        fna_files = list_files(all_fna_file_path,genomes_path,organism)
+    print_status(fna_files)
 
-    #Converttofastq
-    #fq_files=fastq_converter(files)
-    #Getthefqfiles
-    #dirpath=os.getcwd()
-    #os.chdir(dirpath)
-    #fq=get_fq(dirpath)
-    #run_canu(fq)
+    '''Building a dir of fna files'''
+    genomes_fna_path = "{}genomes_parsnp".format(dirpath)
+    create_genomes_dir(genomes_fna_path)
+    os.chdir(genomes_fna_path)
+    for file in fna_files:
+        move_file(file, genomes_fna_path)
+
+    '''Run parsnp'''
+    run_parsnp(dirpath, genomes_fna_path)
+
+
 
 
 #main
