733e8554ebd418bb601c666b862ee17722800fdd
tamascogustavo <gustavo.tamasco@usp.br>
2021-04-08 11:46:10 -0300

    final script

--- snps_stats.py
+++ snps_stats.py
@@ -19,6 +19,7 @@ import itertools
 import shutil
 import math
 import matplotlib
+matplotlib.use("TkAgg")
 from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
 from matplotlib_venn import venn3, venn3_circles
 from matplotlib import pyplot as plt
@@ -70,48 +71,17 @@ def list_files(path):
             files.append(file)
     return (files)
 
-def run_nucmer(comb):
-    '''
-    This function creates the delta files
-    :param query: is a fasta file from a query genome
-    :param ref: is a fasta file from a ref genome
-    :return:
-    '''
-    out_name = "{}_{}".format(comb[0].split(".")[0], comb[1].split(".")[0])
-    out_name_nucmer = "{}.delta".format(out_name)
-    if os.path.exists(out_name_nucmer):
-        print("{} already exists".format(out_name_nucmer))
-    else:
-        cmd_nucmer = "nucmer --prefix={} {} {}".format(out_name, comb[0], comb[1])
-        exit_message = subprocess.check_call(cmd_nucmer, shell=True)
-        print("Exit status: {0}".format(exit_message))
-        print("{0} nucmer was executed".format(out_name))
-    return out_name_nucmer
-
-def run_snp_detection(delta_file):
-    '''
-    This function generates a file with the position where snp happens
-    :param delta_file: file name
-    :return:
-    '''
-    out_name = delta_file.split(".")[0]
-    out_name_snps = "{}.snps".format(out_name)
-    if os.path.exists(out_name_snps):
-        print("{} already exists".format(out_name_snps))
-    else:
-        cmd_snp_view = "show-snps -Clr {} > {}".format(delta_file, out_name_snps)
-        exit_message = subprocess.check_call(cmd_snp_view, shell=True)
-        print("Exit status: {0}".format(exit_message))
-        print("Check the file {0} for snps info".format(out_name))
-    return out_name_snps
-
-
-def generate_combinations(files):
-
-    combinations = itertools.combinations(files,6)
-    return (list(combinations))
 
 def get_total_snp_value(name, file, snp_dict):
+    '''
+    This function calculates the total of snps in a genome comparison
+    :param name: name of the file
+    :param file: is the file
+    :param snp_dict: a dict that will contain the info
+    :return: an updated info
+        :key is the name
+        :value is the total number of snps
+    '''
     size = 0
     for line in file:
         if "NODE" in line:
@@ -120,12 +90,31 @@ def get_total_snp_value(name, file, snp_dict):
     return snp_dict
 
 def print_total_snps(snp_data):
+    '''
+    This function prints the info of the dict of snps
+
+    :param snp_data: dict with snp data
+    :return: none
+    '''
     for k,v in snp_data.items():
         name = k.split(".")[0]
         snp_total = v
         print("The comparison {} has {} as total number of snps ".format(name,snp_total))
 
 def parse_info(name, file, coord_dict):
+    '''
+    This function creates a list with name and update a dict with new name and coordinates
+    of snps
+
+    Here we decided to use log to generate a unique code for each snp position related to
+    the genome reference.
+    Appying log10(ref+query)
+
+    :param name: name of the old file
+    :param file: is the .snps file
+    :param coord_dict: a dict of snp coord associated with name
+    :return: new name
+    '''
     out_name = "{}_coords".format(name.split(".")[0])
     coords_list = []
     for line in file:
@@ -136,17 +125,24 @@ def parse_info(name, file, coord_dict):
             coords_list.append(position_value)
             coord_dict[out_name] = coords_list
         if len(coords_list) == 0:
-            coord_dict[out_name] = 1
+            coord_dict[out_name] = [1]
     return (out_name)
 
 
 def plot_ven(list_names, data):
+    '''
+    This function plots a ven diagram
+
+    :param list_names: list containing 3 names
+    :param data: a dict build previously
+    :return: None
+    '''
 
     list1 = data[list_names[0]]
     list2 = data[list_names[1]]
     list3 = data[list_names[2]]
 
-    venn3([set(list1), set(list2), set(list3)], set_labels=("lol", "show", "hi"))
+    venn3([set(list1), set(list2), set(list3)], set_labels=(list_names[0], list_names[1], list_names[2]))
     plt.show()
 
 
@@ -167,7 +163,7 @@ def main():
     for file in snp_files:
         with open(file) as snp_data:
             get_total_snp_value(file,snp_data, snp_total)
-    print_total_snps(snp_total)
+    #print_total_snps(snp_total)
 
     '''
     Deep stats for ven Diagram
@@ -180,9 +176,9 @@ def main():
             name = parse_info(file_,snp_ven, all_coords)
             all_names.append(name)
     '''
-    The comparison 432 vs 432 is removed, because its comparing to itself
-    # To check unmark the following code
+    Check if something is missing
     '''
+
     for n in all_names:
         if n not in all_coords.keys():
             print(n)
@@ -193,7 +189,8 @@ def main():
     '''
     for i in range(0,len(all_names),3):
         group_for_ven = all_names[i:i+3]
-        plot_ven(group_for_ven, all_coords)
+        print(group_for_ven)
+        #plot_ven(group_for_ven, all_coords)
 
 
 
